6d7cd2211dca591872344264dce9bdc0314ba90a
Volodymyr Kuzmych <vkuzmych@cisco.com>
2024-08-07 09:01:32 +0300

    update api handlers

--- cmd/api/handlers-api.go
+++ cmd/api/handlers-api.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"usual_store/internal/cards"
 )
 
 type stripePayload struct {
@@ -12,20 +14,59 @@ type stripePayload struct {
 
 type jsonResponse struct {
 	OK      bool   `json:"ok"`
-	Message string `json:"message"`
-	Content string `json:"content"`
-	ID      int    `json:"id"`
+	Message string `json:"message,omitempty"`
+	Content string `json:"content,omitempty"`
+	ID      int    `json:"id,omitempty"`
 }
 
 func (app *application) GetPaymentIntent(w http.ResponseWriter, r *http.Request) {
-	j := jsonResponse{
-		OK: true,
-	}
-
-	out, err := json.MarshalIndent(j, "", "  ")
+	var payload stripePayload
+	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
 		app.errorLog.Println(err)
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(out)
+
+	amount, err := strconv.Atoi(payload.Amount)
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+
+	card := cards.Card{
+		Secret:   app.config.stripe.secret,
+		Key:      app.config.stripe.key,
+		Currency: payload.Currency,
+	}
+
+	ok := true
+
+	pi, msg, err := card.Charge(payload.Currency, amount)
+	if err != nil {
+		app.errorLog.Println(err)
+		ok = false
+	}
+
+	if ok {
+		out, err := json.MarshalIndent(pi, "", "	")
+		if err != nil {
+			app.errorLog.Println(err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	} else {
+		j := jsonResponse{
+			OK:      false,
+			Message: msg,
+			Content: "Invalid amount",
+		}
+
+		out, err := json.MarshalIndent(j, "", "  ")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(out)
+	}
+
 }
