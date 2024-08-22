86355e3494b7d14f3d6e9b382c44fda4e0a36ece
Volodymyr Kuzmych <vkuzmych@cisco.com>
2024-08-22 20:15:53 +0300

    update payment methods and template

--- internal/cards/cards.go
+++ internal/cards/cards.go
@@ -3,6 +3,7 @@ package cards
 import (
 	"github.com/stripe/stripe-go/v72"
 	"github.com/stripe/stripe-go/v72/paymentintent"
+	"github.com/stripe/stripe-go/v72/paymentmethod"
 )
 
 type Card struct {
@@ -40,6 +41,26 @@ func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.Payment
 		return nil, msg, err
 	}
 	return pi, "", nil
+}
+
+// GetPaymentMethod gets payment method by payment intent id
+func (c *Card) GetPaymentMethod(s string) (*stripe.PaymentMethod, error) {
+	stripe.Key = c.Secret
+	pm, err := paymentmethod.Get(s, nil)
+	if err != nil {
+		return nil, err
+	}
+	return pm, nil
+}
+
+// RetrievePaymentIntent gets an existing payment intent by id
+func (c *Card) RetrievePaymentIntent(s string) (*stripe.PaymentIntent, error) {
+	stripe.Key = c.Secret
+	pi, err := paymentintent.Get(s, nil)
+	if err != nil {
+		return nil, err
+	}
+	return pi, nil
 }
 
 func cardErrorMessage(code stripe.ErrorCode) string {
