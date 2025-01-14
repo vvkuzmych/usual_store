2cf87360a576ed8cc922979f08a08801a6032d22
vvkyzmych <app.kvv2020@gmail.com>
2025-01-14 10:31:36 +0200

    remove unused code

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -8,17 +8,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-//func OpenDB(dsn string) (*sql.DB, error) {
-//	db, err := sql.Open("mysql", dsn)
-//	if err != nil {
-//		return nil, err
-//	}
-//	if err := db.Ping(); err != nil {
-//		return nil, err
-//	}
-//	return db, nil
-//}
-
 func OpenDB(dsn string) (*sql.DB, error) {
 	// Open the database connection using the postgres driver
 	db, err := sql.Open("postgres", dsn)
@@ -26,14 +15,13 @@ func OpenDB(dsn string) (*sql.DB, error) {
 		fmt.Println("got error")
 		return nil, err
 	}
-	fmt.Println("success to open")
-	fmt.Println("db+++++", db)
+
 	// Ping the database to ensure the connection is established
 	if err := db.Ping(); err != nil {
 		fmt.Println("no ping")
 		return nil, err
 	}
-	fmt.Println("success to ping")
+
 	// Return the connection object
 	fmt.Println("Successfully connected to database")
 	return db, nil
