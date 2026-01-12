943794e00b3902787c24b48434b532cd956acc2c
Volodymyr Kuzmych <44097750+vvkuzmych@users.noreply.github.com>
2026-01-12 17:17:45 +0200

    add tests for customer_model and refactoring repository tests

--- internal/models/models.go
+++ internal/models/models.go
@@ -81,8 +81,8 @@ type Transaction struct {
 type User struct {
 	ID        int       `json:"id"`
 	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name" validate:"required"`
-	Email     string    `json:"email" validate:"required,email"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	Role      string    `json:"role"`
 	CreatedAt time.Time `json:"created_at"`
